5207306ad68bb709a422ac5b400ac1d25910fe9f
Yves Plansson <yclpjr@gmail.com>
2020-07-12 22:39:23 -0300

    Atualiza Frete em Massa por Categoria/SubCategoria

--- app/admin/frete.php
+++ app/admin/frete.php
@@ -8,6 +8,7 @@ class Frete extends PHPFrodo
     public $user_name;
     public $user_level;
     public $frete_param;
+    public $linhas_afetadas = 0;
 
     public function __construct()
     {
@@ -38,7 +39,8 @@ class Frete extends PHPFrodo
         }
         if ( $this->user_level == 1 ) {
             $this->assign('showhide','hide');
-        }         
+        }
+
     }
 
     public function welcome()
@@ -84,6 +86,46 @@ class Frete extends PHPFrodo
             $this->update( 'frete' )->set()->execute();
             $this->redirect( "$this->baseUri/admin/frete/process-ok/" );
         }
+    }
+
+    public function fillCategoria() {
+        $this->select()
+            ->from('categoria')
+            ->orderby('categoria_title asc')
+            ->execute();
+        if ($this->result()) {
+            $this->fetch('combo', $this->data);
+        }
+    }
+
+    public function bulkupdate() {
+        //$this->pagebase = "$this->baseUri/admin/item";
+        if (isset($_POST['item_categoria'])) {
+            $item_categoria = $_POST['item_categoria'];
+            $where  = "item_categoria = $item_categoria";
+        }
+        if (isset($_POST['item_sub']) && $_POST['item_sub'] != 0) {
+            $item_sub = (int) $_POST['item_sub'];
+            $where .= " AND item_sub = $item_sub";
+        }
+        if (isset($_POST['item_calcula_frete'])) {
+            $item_calcula_frete = $_POST['item_calcula_frete'];
+        }
+        if ($this->postIsValid(array(
+            'item_categoria' => 'string'
+        ))) {
+            $this->select()->from('item')->where($where)->execute();
+            if($this->result()){
+                $this->assign('linhas_afetadas', count($this->data) . ' linha(s) atualizada(s).');
+                $this->linhas_afetadas = count($this->data) . ' linha(s) atualizada(s).';
+                $this->update('item')->set(array('item_calcula_frete'),array($item_calcula_frete))->where($where)->execute();
+            }
+            $this->redirect("$this->baseUri/admin/frete/bulkupdate/process-ok/");
+        }
+        //var_dump($this);
+        $this->tpl('admin/freight_bulk_update.html');
+        $this->fillCategoria();
+        $this->render();
     }
 
 }
